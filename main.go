f676f170d2c1f06c9cb4a35be129c08ac4645447
Ethan Water <watermonic@gmail.com>
2023-12-18 15:19:04 -0500

    image validity handlinh

--- main.go
+++ main.go
@@ -1,62 +1,80 @@
-package main 
+package main
 
 import (
-  "context"
-  "os"
-  "fmt"
+	"context"
+	"fmt"
+	"os"
+	"strings"
 
-  vision "cloud.google.com/go/vision/apiv1"
-  pb "google.golang.org/genproto/googleapis/cloud/vision/v1"
+	vision "cloud.google.com/go/vision/apiv1"
+	pb "google.golang.org/genproto/googleapis/cloud/vision/v1"
 )
 
 var chunkSize = 2048
 
+func IsValidImage(imagePath string) bool {
+	switch {
+  case strings.HasPrefix(imagePath, "."):
+    return false
+	case
+		strings.HasSuffix(imagePath, ".jpg"),
+		strings.HasSuffix(imagePath, ".jpeg"),
+		strings.HasSuffix(imagePath, ".png"),
+		strings.HasSuffix(imagePath, ".gif"),
+		strings.HasSuffix(imagePath, ".bmp"):
+		return true
+  default:
+    return false
+	}
+}
+
 func ObtainImageLabels(ctx context.Context, imagePath string) {
-  f, err := os.Open(fmt.Sprintf("bucket/%s", imagePath))
-  if err != nil {
-    fmt.Printf("%s", err)
-    return
-  }
-  defer f.Close()
+	f, err := os.Open(fmt.Sprintf("bucket/%s", imagePath))
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
+	defer f.Close()
 
-  client, err := vision.NewImageAnnotatorClient(ctx)
-  if err != nil {
-    fmt.Printf("%s", err)
-    return
-  }
-  defer client.Close()
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
+	defer client.Close()
 
-  image, err := vision.NewImageFromReader(f)
-  if err != nil {
-    fmt.Printf("%s", err)
-    return
-  }
+	image, err := vision.NewImageFromReader(f)
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
 
-  entityAnnotations, err := client.DetectLabels(ctx, image, &pb.ImageContext{}, 10)
-  if err != nil {
-    fmt.Printf("%s", err)
-    return
-  } 
+	entityAnnotations, err := client.DetectLabels(ctx, image, &pb.ImageContext{}, 10)
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
 
-  for _, entity := range entityAnnotations {
-    fmt.Println(entity)
-  }
-
+	for _, entity := range entityAnnotations {
+		fmt.Println(entity)
+	}
 }
 
 func main() {
-  ctx := context.Background()
+	ctx := context.Background()
 
-  bucket, err := os.ReadDir("bucket")
-  if err != nil {
-    fmt.Printf("%s", err)
-    return
-  }
+	bucket, err := os.ReadDir("bucket")
+	if err != nil {
+		fmt.Printf("%s", err)
+		return
+	}
 
-  for _, object := range bucket {
-    fmt.Println(object.Name())
-    ObtainImageLabels(ctx, object.Name())
-    fmt.Println()
-  }
+	for _, object := range bucket {
+    if IsValidImage(object.Name()) {
+		  fmt.Println(object.Name())
+		  ObtainImageLabels(ctx, object.Name())
+    }
+		fmt.Println()
+	}
 
 }
